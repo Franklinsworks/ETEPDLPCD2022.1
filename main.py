d095a7b50601a2a3878d15e11080e72d732728ac
Franklinsworks <100326404+Franklinsworks@users.noreply.github.com>
2022-03-09 19:24:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#ETE PORTO DIGITAL
+#PROFESSOR CLOVES
+#ALUNO TARCÍSIO FRANKLIN
+while(True):
+  selecionar=int(input("digite um valor"))
+  if (selecionar==1):
+    print("acelerar")
+  elif selecionar==2:
+    print("decolar")
+  elif selecionar==3:
+    print("planar")
+  elif selecionar==4:
+    print("Função piloto automatico")
+  elif selecionar==5:
+    print("pousar")
